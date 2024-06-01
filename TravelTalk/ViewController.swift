ca1a803025d2769f89225ccd50de735091d665f1
jeongyun-kim <kkanyo04@naver.com>
2024-06-01 15:24:21 +0900

    Initial Commit

---
+++ TravelTalk/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TravelTalk
+//
+//  Created by 김정윤 on 6/1/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
